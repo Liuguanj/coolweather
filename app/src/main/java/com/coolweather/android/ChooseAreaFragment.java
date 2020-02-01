e007f5e7b3e4bbec05b3e2522bb0671755035ee5
liuguanj <liuguanj@gmail.com>
2020-02-01 15:21:59 +0800

    加入显示天气信息的功能。

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -109,8 +109,12 @@ public class ChooseAreaFragment extends Fragment {
                     selectedCity = cityList.get(position);
                     queryCounties();
                 }
-//                else if (currentLevel == LEVEL_COUNTY) {
-//                    String weatherId = countyList.get(position).getWeatherId();
+                else if (currentLevel == LEVEL_COUNTY) {
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id", weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
 //                    if (getActivity() instanceof MainActivity) {
 //                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
 //                        intent.putExtra("weather_id", weatherId);
@@ -122,7 +126,7 @@ public class ChooseAreaFragment extends Fragment {
 //                        activity.swipeRefresh.setRefreshing(true);
 //                        activity.requestWeather(weatherId);
 //                    }
-//                }
+                }
             }
         });
         backButton.setOnClickListener(new View.OnClickListener() {
